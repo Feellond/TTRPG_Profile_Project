35db6bec8a50428c93a7995ad2134c1d05ca6b59
Иван Харсекин <forfeellond@mail.ru>
2023-10-13 15:27:23 +0700

    Добавил в DBContext сущности

--- TTRPG_Project.DAL/Data/ApplicationDbContext.cs
+++ TTRPG_Project.DAL/Data/ApplicationDbContext.cs
@@ -3,6 +3,10 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
 using TTRPG_Project.DAL.Entities.Database;
+using TTRPG_Project.DAL.Entities.Database.Additional;
+using TTRPG_Project.DAL.Entities.Database.Creatures;
+using TTRPG_Project.DAL.Entities.Database.Items;
+using TTRPG_Project.DAL.Entities.Database.Spells;
 using TTRPG_Project.DAL.Entities.Database.Users;
 using TTRPG_Project.DAL.Entities.Interface;
 using TTRPG_Project.DAL.Repositories;
@@ -11,8 +15,38 @@ namespace TTRPG_Project.DAL.Data
 {
     public class ApplicationDbContext : IdentityDbContext<User, Role, string>, IDbRepository
     {
-        public DbSet<ServiceLog> ServiceLogs { get; set; }
+        public DbSet<Effect> Effects { get; set; }
+        public DbSet<ServicePrice> ServicePrices { get; set; }
+        public DbSet<Abilitiy> Abilitiys { get; set; }
+        public DbSet<Attack> Attacks { get; set; }
+        public DbSet<AttackEffectList> AttackEffectList { get; set; }
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<Creature> Creatures { get; set; }
+        public DbSet<CreatureEffectList> CreatureEffectList { get; set; }
+        public DbSet<CreatureRewardList> CreatureRewardList { get; set; }
+        public DbSet<Race> Races { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<SkillsList> SkillsList { get; set; }
+        public DbSet<SkillsTree> SkillsTree { get; set; }
+        public DbSet<Stat> Stats { get; set; }
+        public DbSet<StatsList> StatsList { get; set; }
+        public DbSet<AlchemicalItem> AlchemicalItems { get; set; }
+        public DbSet<Armor> Armors { get; set; }
+        public DbSet<Blueprint> Blueprints { get; set; }
+        public DbSet<BlueprintComponentList> BlueprintComponentList { get; set; }
+        public DbSet<Component> Components { get; set; }
+        public DbSet<Formula> Formulas { get; set; }
+        public DbSet<FormulaComponentList> FormulaComponentList { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<ItemBase> ItemBases { get; set; }
+        public DbSet<ItemBaseEffectList> ItemBaseEffectList { get; set; }
+        public DbSet<Tool> Tools { get; set; }
+        public DbSet<Weapon> Weapons { get; set; }
+        public DbSet<Spell> Spells { get; set; }
+        public DbSet<SpellComponentList> SpellComponentList { get; set; }
+        public DbSet<SpellSkillProtectionList> SpellSkillProtectionList { get; set; }
         public DbSet<Headline> Headlines { get; set; }
+        public DbSet<ServiceLog> ServiceLogs { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
