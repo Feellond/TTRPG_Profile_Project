385d8f8f1853785d1ccf72a52121960a8afd0d3c
Иван Харсекин <forfeellond@mail.ru>
2023-10-16 17:49:58 +0700

    Создание контроллеров и добавление базовых методов по alchemicalItem

--- TTRPG_Profile_Project/Controllers/ItemController.cs
+++ TTRPG_Profile_Project/Controllers/ItemController.cs
@@ -1,19 +1,144 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using TTRPG_Project.BL.Const;
+using TTRPG_Project.BL.Services.Items;
+using TTRPG_Project.DAL.Entities.Database.Items;
 
 namespace TTRPG_Project.Web.Controllers
 {
-    [Authorize(Roles = nameof(Roles.MODERATOR) + "," + nameof(Roles.ADMINISTRATOR))]
+    //[Authorize(Roles = nameof(Roles.MODERATOR) + "," + nameof(Roles.ADMINISTRATOR))]
+    [Authorize]
     [ApiController]
-    [Route("api/item")]
+    [Route("api/items")]
     public class ItemController : ControllerBase
     {
-        [HttpGet]
-        public async Task<IActionResult> GetAllItems()
-        {
+        #region Инициализация контроллера
+        private readonly AlchemicalItemService _alchemicalItemService;
+        private readonly ArmorService _armorService;
+        private readonly BlueprintService _blueprintService;
+        private readonly ComponentService _componentService;
+        private readonly FormulaService _formulaService;
+        private readonly ItemBaseService _itemBaseService;
+        private readonly ItemService _itemService;
+        private readonly ToolService _toolService;
+        private readonly WeaponService _weaponService;
 
-            return Ok();
+        public ItemController(
+            AlchemicalItemService alchemicalItemService,
+            ArmorService armorService,
+            BlueprintService blueprintService,
+            ComponentService componentService,
+            FormulaService formulaService,
+            ItemBaseService itemBaseService,
+            ItemService itemService, 
+            ToolService toolService, 
+            WeaponService weaponService
+            )
+        {
+            _alchemicalItemService = alchemicalItemService;
+            _armorService = armorService;
+            _blueprintService = blueprintService;
+            _componentService = componentService;
+            _formulaService = formulaService;
+            _itemBaseService = itemBaseService;
+            _itemService = itemService;
+            _toolService = toolService;
+            _weaponService = weaponService;
+        }
+        #endregion
+
+        #region Алхимия (зельки и предметы)
+        [HttpGet("alchemicalitem")]
+        public async Task<IActionResult> GetAlchemicalItems()
+        {
+            var result = await _alchemicalItemService.GetAllAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("alchemicalitem/{id}")]
+        public async Task<IActionResult> GetAlchemicalItem([FromRoute] int id)
+        {
+            var result = await _alchemicalItemService.GetByIdAsync(id);
+            return Ok(result);
+        }
+
+        [HttpPost("alchemicalitem")]
+        public async Task<IActionResult> CreateAlchemicalItem(AlchemicalItem alchemicalItem)
+        {
+            var result = await _alchemicalItemService.CreateAsync(alchemicalItem);
+            return Ok(result);
+        }
+
+        [HttpPut("alchemicalitem")]
+        public async Task<IActionResult> UpdateAlchemicalItem(AlchemicalItem alchemicalItem)
+        {
+            var result = await _alchemicalItemService.UpdateAsync(alchemicalItem);
+            return Ok(result);
+        }
+
+        [HttpDelete("alchemicalitem")]
+        public async Task<IActionResult> DeleteAlchemicalItem(int alchemicalItemId)
+        {
+            var item = await _alchemicalItemService.GetByIdAsync(alchemicalItemId);
+            var result = await _alchemicalItemService.DeleteAsync(item);
+            return Ok(result);
+        }
+        #endregion
+
+        [HttpGet("armor")]
+        public async Task<IActionResult> GetArmors()
+        {
+            var result = await _armorService.GetAllAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("blueprint")]
+        public async Task<IActionResult> GetBlueprints()
+        {
+            var result = await _blueprintService.GetAllAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("component")]
+        public async Task<IActionResult> GetComponents()
+        {
+            var result = await _componentService.GetAllAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("formula")]
+        public async Task<IActionResult> GetFormulas()
+        {
+            var result = await _formulaService.GetAllAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("base")]
+        public async Task<IActionResult> GetBaseItems()
+        {
+            var result = await _itemBaseService.GetAllAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("item")]
+        public async Task<IActionResult> GetItems()
+        {
+            var result = await _itemService.GetAllAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("tool")]
+        public async Task<IActionResult> GetTools()
+        {
+            var result = await _toolService.GetAllAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("weapon")]
+        public async Task<IActionResult> GetWeapons()
+        {
+            var result = await _weaponService.GetAllAsync();
+            return Ok(result);
         }
     }
 }
---
+++ TTRPG_Profile_Project/Controllers/SpellController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TTRPG_Project.BL.Const;
+using TTRPG_Project.BL.Services.Items;
+
+namespace TTRPG_Project.Web.Controllers
+{
+    //[Authorize(Roles = nameof(Roles.MODERATOR) + "," + nameof(Roles.ADMINISTRATOR))]
+    [Authorize]
+    [ApiController]
+    [Route("api/spells")]
+    public class SpellController : ControllerBase
+    {
+        
+    }
+}
