630773c6b7c4c0ee0dfc7ef8a52a8f96b8fed077
Иван Харсекин <forfeellond@mail.ru>
2024-04-01 10:40:04 +0700

    Исправлен warning. Бэкап 2

--- TTRPG_Profile_Project/ClientApp/src/app/hoc/routeAnonymous.tsx
+++ TTRPG_Profile_Project/ClientApp/src/app/hoc/routeAnonymous.tsx
@@ -1,9 +1,4 @@
-import { Context } from "index";
-import React, { useContext } from "react";
-
 const RouteAnonymous = ({ jsx }) => {
-  const { store } = useContext(Context);
-
   return jsx;
 };
 
